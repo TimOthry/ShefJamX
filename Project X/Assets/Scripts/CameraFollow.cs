0d3b51e4bb907738b93a6793d94b54781fe09589
Tom <trfoster1794@gmail.com>
2024-02-09 23:09:26 +0000

    added camera script and player movement

---
+++ Project X/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Project X/Assets/Scripts/PlayerBehaviour.cs
+++ Project X/Assets/Scripts/PlayerBehaviour.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    private Vector2 movePosition = Vector2.zero;
+    private Rigidbody2D rigidBody;
+    [SerializeField]
+    private float moveSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = GetMousePos();
+        movePosition = Vector2.Lerp(transform.position, mousePos, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.MovePosition(movePosition);
+    }
+
+    private Vector3 GetMousePos()
+    {
+        return mainCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 }
