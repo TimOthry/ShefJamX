b9435c1961f57038620a46c4a97ed584a15e5e79
TimOthry <timsonlee3@gmail.com>
2024-02-10 01:48:37 +0000

    Bullet constant firing

--- Project X/Assets/Scripts/Weapon.cs
+++ Project X/Assets/Scripts/Weapon.cs
@@ -7,10 +7,15 @@ public class Weapon : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
 
+    [SerializeField]
+    private float fireRate = 0.005f;
+    private float nextFire = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) {
+        if (Input.GetButton("Fire1") && Time.time > nextFire) {
+            nextFire = Time.time + fireRate;
             Shoot();
         }
     }
