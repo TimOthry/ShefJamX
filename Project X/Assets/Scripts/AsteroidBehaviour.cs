69c31650d0f3e62e495624a5bc8aad291d9a1f52
Tanay Patel <tvpatel1@sheffield.ac.uk>
2024-02-10 17:22:10 +0000

    Working on Asteriod movement

--- Project X/Assets/Scripts/AsteroidBehaviour.cs
+++ Project X/Assets/Scripts/AsteroidBehaviour.cs
@@ -7,6 +7,26 @@ public class AsteroidBehaviour : MonoBehaviour
     public int health = 100;
 
     public GameObject destructionEffect;
+    private GameObject player;
+
+    [SerializeField] private float Angle;
+    public Vector3 vectorVelocity;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        float y = gameObject.transform.position.y - player.transform.position.y;
+        float x = gameObject.transform.position.x - player.transform.position.x;
+        Angle = Mathf.Atan2(y, x) * Mathf.Rad2Deg;
+        //Angle += Random.Range(-30, 30);
+    }
+
+    private void Update()
+    {
+        Vector2 movement = Quaternion.Euler(0, 0, Angle) * Vector2.up;
+        vectorVelocity = movement * Time.deltaTime * 5;
+        transform.position += vectorVelocity;
+    }
 
     public void TakeDamage(int damage)
     {
@@ -17,7 +37,8 @@ public class AsteroidBehaviour : MonoBehaviour
             Die();
         }
     }
-    
+ 
+
     void Die()
     {
         Instantiate(destructionEffect, transform.position, Quaternion.identity);
