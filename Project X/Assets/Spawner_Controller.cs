6f9e7ebac010daa25747bdf58fb74d6015df4da8
Tanay Patel <tvpatel1@sheffield.ac.uk>
2024-02-10 14:47:15 +0000

    asteriod spawning

---
+++ Project X/Assets/Spawner_Controller.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spawner_Controller : MonoBehaviour
+{
+
+    public GameObject asteriod;
+    public GameObject home;
+    private Transform playerPos;
+    [SerializeField] private float spawnRate;
+    [SerializeField] private float distance;
+    [SerializeField] private bool isSpawning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnRate = 5;
+        Transform homePos = home.transform;
+        playerPos = GameObject.Find("Player").transform;
+
+        if (!isSpawning)
+        {
+            StartCoroutine(Asteriod_Spawn_Timer(playerPos));
+        }
+        
+        //Vector2 distanceVector = playerPos.position - homePos.position;
+        //distance = Mathf.Pow(Mathf.Pow(Mathf.Abs(distanceVector.x), 2) + Mathf.Pow(Mathf.Abs(distanceVector.y), 2), 0.5f);
+
+        //if (spawnRate > 0.3f)
+        //{
+        //    spawnRate -= Mathf.Floor(distance / 500) * 0.5f;
+        //}
+        //else
+        //{
+        //    spawnRate = 0.3f;
+        //}
+        
+
+
+    }
+
+    IEnumerator Asteriod_Spawn_Timer(Transform playerPos)
+    {
+        isSpawning = true;
+        yield return new WaitForSeconds(spawnRate);
+        Spawn_Asteriod(playerPos);
+        isSpawning = false;
+    }
+
+    private void Spawn_Asteriod(Transform pos) {
+
+        Vector2 v2Pos = Convert_To_V2(pos);
+
+        Vector2 randomPos = v2Pos + UnityEngine.Random.insideUnitCircle.normalized * 22;
+
+        GameObject asteriodInstance = Instantiate(asteriod, randomPos, Quaternion.identity);
+
+    }
+
+    private Vector2 Convert_To_V2(Transform pos)
+    {
+        Vector2 v2 = new Vector2(pos.position.x, pos.position.y);
+        return v2;
+    }
+
+
+
+}
