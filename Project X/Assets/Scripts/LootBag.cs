26cc9b670a6c06841901e6edf145f48a1d29de0c
TimOthry <timsonlee3@gmail.com>
2024-02-10 18:54:41 +0000

    Added loot prefabs and Lootbag script

---
+++ Project X/Assets/Scripts/LootBag.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBag : MonoBehaviour
+{
+    public GameObject droppedItemPrefab;
+    public List<Loot> lootlist = new List<Loot>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
