a9363daac8ef0ed74e10c5c7a660d31cffad48e6
ReuBird23 <150720326+ReuBird23@users.noreply.github.com>
2024-02-11 15:56:08 +0000

    Commented two "using"s that prevented building

--- Project X/Assets/Scripts/PlayerBehaviour.cs
+++ Project X/Assets/Scripts/PlayerBehaviour.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Animation;
+//using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.Serialization;
 
--- Project X/Assets/Scripts/Starfield.cs
+++ Project X/Assets/Scripts/Starfield.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.VersionControl;
+//using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class Starfield : MonoBehaviour
